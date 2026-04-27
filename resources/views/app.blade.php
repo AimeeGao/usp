d8c4c357cc9df4d8882ad431f9726f7ab798fa42
AimeeGao <Aimee.Gao@gov.bc.ca>
2026-04-27 13:42:23 -0700

    process static assets with Vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,8 +15,8 @@
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-        <link rel="icon" href="{{ asset('/images/favicon.ico') }}" type="image/vnd.microsoft.icon">
-        <link rel="icon" type="image/x-icon" href="{{ asset('/images/favicon.ico') }}">
+        <link rel="icon" href="{{ Vite::asset('resources/images/favicon.ico') }}" type="image/vnd.microsoft.icon">
+        <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/images/favicon.ico') }}">
 
         @inertiaHead
     </head>
